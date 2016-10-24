a670a693cc478c1466375944101092a72af94d93
joeDiHare <stefano286@gmail.com>
2016-10-24 09:28:28 +0100

    more of the same

--- WhatChat_v2.py
+++ WhatChat_v2.py
@@ -643,6 +643,19 @@ os.system("open OutputAnalysis.html")
 # import pdfkit
 # with open('OutputAnalysis.html') as f:
 #     pdfkit.from_file(f, 'out.pdf')
+from PyQt5.QtGui import QTextDocument, QPrinter, QApplication
+import sys
+app = QApplication(sys.argv)
+doc = QTextDocument()
+location = "c://apython//Jim//html//notes.html"
+html = open(location).read()
+doc.setHtml(html)
+printer = QPrinter()
+printer.setOutputFileName("foo.pdf")
+printer.setOutputFormat(QPrinter.PdfFormat)
+printer.setPageSize(QPrinter.A4);
+printer.setPageMargins (15,15,15,15,QPrinter.Millimeter);
+doc.print_(printer)
 
 # To do:
 # Module to: Check how words are stretched as in informal conversations
